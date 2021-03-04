a426eb449b4cf320bb944aade6d736dd9610dec2
larlarlar <larisa.gurina.s@gmail.com>
2021-03-04 21:07:58 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,30 @@
+package ru.netology.domain;
+
+import java.util.Objects;
+
+public class Book extends Product {
+    private String author;
+
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (super.matches(product, search)) {
+            return true;
+        }
+        if (product instanceof Book) {
+            Book book = (Book) product;
+            if (book.getAuthor().equalsIgnoreCase(search)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
